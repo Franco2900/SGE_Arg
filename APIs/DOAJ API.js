8de639c9f2c0764e67980d82f7ecac04336d4218
Franco2900 <franco2900@gmail.com>
2023-09-04 19:56:12 -0300

    Añadido los scripts para Latindex y DOAJ

--- APIs/DOAJ API.js
+++ APIs/DOAJ API.js
@@ -1,11 +1,10 @@
-function extraerInfoDOAJ(XMLHttpRequest, fs)
+function extraerInfoDOAJ(XMLHttpRequest, fs, paginaActual = 1, info = "Titulo; E-ISSN;P-ISSN;Institución;Editora;\n")
 {    
     const API_URL = "https://doaj.org/api/"; // URL a la que vamos a pedir los datos
 
     const xhttp = new XMLHttpRequest(); // Creo un objeto de la clase XMLHttpRequest para poder hacer el intercambio de datos
 
-    // BUSCAR COMO HACER MEJOR LA CONSULTA PORQUE SOLO TIRA 100 REVISTAS DE LAS 381 (QUIZAS HACER VARIAS CONSULTAS PORQUE EL LIMITE ES 100)
-    xhttp.open("GET", `${API_URL}/search/journals/Argentina?page=1&pageSize=100`, true); // Crea la solicitud al servidor
+    xhttp.open("GET", `${API_URL}/search/journals/Argentina?page=${paginaActual}&pageSize=100`, true); // Crea la solicitud al servidor
     xhttp.send();   // Envía la solicitud al servidor
 
     // Recibo la respuesta del servidor
@@ -14,28 +13,68 @@ function extraerInfoDOAJ(XMLHttpRequest, fs)
         // El atributo readyState es el estado de un objeto de la clase XMLHttpRequest, y el atributo status define el estado de una solicitud al servidor
         if (this.readyState == 4 && this.status == 200) // cuando esta condición se cumple, significa que la respuesta del servidor ya se recibió y que no hubo problemas
         { 
-            // response es la respuesta del servidor
-            const respuestaJSON = JSON.parse(this.response); // las APIs se suelen comunicar en JSON, así que parseo la respuesta a JSON
-            info = "";
+            console.log("Comienza la extracción de datos de DOAJ");
 
-            // Filtro la info recibida
-            for(let i = 0; i < respuestaJSON.pageSize; i++)
+            // response es la respuesta del servidor
+            const respuestaJSON = JSON.parse(this.response); // Las APIs se suelen comunicar en JSON, así que parseo la respuesta a JSON
+        
+            // Ya que cada consulta solo puede devolver una página con un máximo de 100 revistas, me fijo cuantas páginas me falta consultar
+            var cantidadPaginas = Math.ceil(respuestaJSON.total/respuestaJSON.pageSize);
+            var limite = respuestaJSON.pageSize;
+            if(paginaActual == cantidadPaginas) limite = respuestaJSON.total - (respuestaJSON.pageSize * (--cantidadPaginas));
+
+            // Filtro la info recibida por la API
+            console.log(`PÁGINA: ${paginaActual}`);
+            for(let i = 0; i < limite; i++)
             {
-                info += "Titulo: " + respuestaJSON.results[i].bibjson.title + "\n";
-                console.log(`Titulo ${i}: ${respuestaJSON.results[i].bibjson.title}`);
+                var titulo = respuestaJSON.results[i].bibjson.title.trim().replaceAll(";", ",");
+
+                // No todas las revistas tienen todos los datos, así que tengo que verificar si tienen ciertos datos o no
+                var eissn;
+                if(typeof(respuestaJSON.results[i].bibjson.eissn) == "undefined") eissn = null;
+                else                                                              eissn = respuestaJSON.results[i].bibjson.eissn.trim().replaceAll(";", ",");
+
+                var pissn;
+                if(typeof(respuestaJSON.results[i].bibjson.pissn) == "undefined") pissn = null;
+                else                                                              pissn = respuestaJSON.results[i].bibjson.pissn.trim().replaceAll(";", ",");
+
+                var nombreInstituto;
+                if     (typeof(respuestaJSON.results[i].bibjson.institution) == "undefined")      nombreInstituto = null;
+                else if(typeof(respuestaJSON.results[i].bibjson.institution.name) == "undefined") nombreInstituto = null;
+                else nombreInstituto = respuestaJSON.results[i].bibjson.institution.name.trim().replaceAll(";", ",");
+
+                var editora;
+                if     (typeof(respuestaJSON.results[i].bibjson.publisher) == "undefined")      editora = null;
+                else if(typeof(respuestaJSON.results[i].bibjson.publisher.name) == "undefined") editora = null;
+                else                                                                            editora = respuestaJSON.results[i].bibjson.publisher.name.trim().replaceAll(";", ",");
+
+                info += `${titulo};${eissn};${pissn};${nombreInstituto};${editora};\n`;
+
+                console.log(`***********************************************************************************`);
+                console.log(`Titulo ${i}: ${titulo}`);
+                console.log(`E-ISSN: ${eissn}`);
+                console.log(`P-ISSN: ${pissn}`);
+                console.log(`Institucion: ${nombreInstituto}`);
+                console.log(`Editora: ${editora}`);
+                console.log(`***********************************************************************************`);
             }
-            
-            // Escribo la info filtrada en el archivo .xls
+
+            // Si no termine de consultar todas las páginas, vuelvo a hacer la consulta pero en la página siguiente y con la info que ya obtuvimos
+            if(paginaActual != ++cantidadPaginas) extraerInfoDOAJ(XMLHttpRequest, fs, ++paginaActual, info); 
+
+            // Escribo la info en el archivo .xls
             fs.writeFile('./Revistas/DOAJ.xls', info, error => 
             { 
                 if(error) console.log(error);
             })
 
             //ESTO ES PARA DEBUGEAR, GUARDA EL JSON CRUDO EN UN TXT ASI LO PONEMOS DESPUES EN JSONVIEWER
-            fs.writeFile('./Revistas/DOAJ.txt', this.response, error => 
+            /*fs.writeFile('./Revistas/DOAJ.txt', this.response, error => 
             { 
                 if(error) console.log(error);
-            })
+            })*/
+
+            console.log("Termina la extracción de datos de DOAJ");
         }
     };
 }
--- modulos webScrapping/Latindex webScrapping.js
+++ modulos webScrapping/Latindex webScrapping.js
@@ -1,77 +1,161 @@
-// Se usan dos funciones para hacer el web scrapping:
-// extraerInfoLatindex() hace web scrapping en la barra de búsqueda de Latindex
-// extraerInfoRevista() hace web scrapping de cada revista en la barra de búsqueda
-
-async function extraerInfoLatindex(puppeteer, jsdom, fs)
+// Busco cuantas páginas devuelve la consulta a Latindex (cada página tiene entre 1 y 20 revistas)
+async function buscarCantidadPaginas(puppeteer, jsdom)
 {
-  console.log("INICIA EL WEB SCRAPPING");
-  
   try 
   {
     const browser  = await puppeteer.launch({headless: 'new'}); // inicio puppeter
-    var paginaActual = 1;
-    var info = "";
-    
-    // Hago el web scrapping en la barra de búsqueda de Latindex
-    while(paginaActual <= 23)  // ARREGLAR, ESTA HARDCODEADO, NO SERVIRIA SI HAY MÁS REVISTAS EN EL FUTURO
-    {
-      console.log(`PÁGINA ${paginaActual}`);
-  
-      const page     = await browser.newPage(); 
-      const response = await page.goto(`https://www.latindex.org/latindex/bAvanzada/resultado?idMod=1&tema=0&subtema=0&region=0&pais=3&critCat=0&send=Buscar&page=${paginaActual}`); // URL del sitio web al que se le hace web scrapping
-      const body     = await response.text(); // Extraigo todo el HTML extraido y lo guardo en esta variable  
+
+    const page     = await browser.newPage();
+    page.setDefaultNavigationTimeout(0);    // Quita el límite de tiempo para conectarse a un sitio web 
+    const response = await page.goto(`https://www.latindex.org/latindex/bAvanzada/resultado?idMod=1&tema=0&subtema=0&region=0&pais=3&critCat=0&send=Buscar&page=1`); // URL del sitio web al que se le hace web scrapping
+    const body     = await response.text(); // Guardo el HTML extraido en esta variable  
             
-      const { window: { document } } = new jsdom.JSDOM(body); // inicio JSDOM y le paso el HTML extraido
-        
-      document.querySelectorAll('div table tbody tr td div a').forEach(element => // Busqueda de la info que queremos. Con el querySelectorAll() hacemos filtro al HTML extraido
-      { 
-        info += "Título: " + element.textContent.trim() + "\n";   
-        console.log("Título: " + element.textContent.trim() );
-        //extraerInfoRevista(browser, jsdom, element.getAttribute("href") );
-      });
-  
-      paginaActual++;
-    }
-  
-    // Escribimos la info filtrada en el archivo excel. En caso de que ya exista el archivo, lo reescribe así tenemos siempre la información actualizada
-    fs.writeFile('./Revistas/Latindex.xls', info, error => 
-    { 
-      if(error) console.log(error);
-    })
-  
-    await browser.close();  // cierro puppeter
+    const { window: { document } } = new jsdom.JSDOM(body); // inicio JSDOM y le paso el HTML extraido
+
+    const filtroHTML      = document.querySelectorAll('nav ul li a');              // Hago un filtro al HTML extraido
+    const cantidadPaginas = parseInt(filtroHTML[filtroHTML.length-2].textContent); // Extraigo la información que quiero del filtro
+
+    await browser.close(); // cierro puppeter
+
+    console.log("CANTIDAD DE PÁGINAS: " + cantidadPaginas);
+    return cantidadPaginas;
   }
   catch (error) 
   {
     console.error(error);
   }
-  
-  console.log("TERMINA EL WEB SCRAPPING"); 
 }
 
 
-// No ejecutar hasta encontrar la forma de que no cuelgue la pc o buscar otro modo de extraer la info de cada revista
-async function extraerInfoRevista(browser, jsdom, URL){
+// Busco los enlaces de cada revista que devuelva la consulta a Latindex
+async function buscarEnlacesARevistas(puppeteer, jsdom)
+{  
   try 
-  {    
-    var info = "";
+  {
+    const browser  = await puppeteer.launch({headless: 'new'}); 
+    var paginaActual = 1;
+    const cantidadPaginas = await buscarCantidadPaginas(puppeteer, jsdom);
+    var enlaces = [];
+    
+    while(paginaActual <=  cantidadPaginas) 
+    {
+      console.log(`Obteniendo enlaces de la página ${paginaActual}`);
   
-    const page     = await browser.newPage(); 
-    const response = await page.goto(URL); 
-    const body     = await response.text();
+      const page     = await browser.newPage(); 
+      page.setDefaultNavigationTimeout(0);
+      const response = await page.goto(`https://www.latindex.org/latindex/bAvanzada/resultado?idMod=1&tema=0&subtema=0&region=0&pais=3&critCat=0&send=Buscar&page=${paginaActual}`); 
+      const body     = await response.text();   
             
-    const { window: { document } } = new jsdom.JSDOM(body);
+      const { window: { document } } = new jsdom.JSDOM(body); 
         
-    document.querySelectorAll("#rev-linea table tbody tr td").forEach(element => // FALTA PERFECCIONAR EL FILTRO
-    { 
-      //info += "ISSN: " + element.textContent.trim();  
-      console.log("ISSN: " + element.textContent.trim() ); 
-    });
-     
+      const filtroHTML =document.querySelectorAll('div table tbody tr td div a');
+      
+      filtroHTML.forEach(element => 
+      { 
+        enlaces.push(element.getAttribute("href") );   
+      });
+  
+      paginaActual++;
+    }
+  
+    await browser.close();
+    return enlaces;  
   }
-  catch (error) {
+  catch (error) 
+  {
     console.error(error);
   }
+}
+
+
+// Extraigo la info de una revista
+async function extraerInfoRevista(puppeteer, jsdom, enlace)
+{  
+  try 
+  {
+    const browser  = await puppeteer.launch({headless: 'new'}); 
+  
+    const page     = await browser.newPage(); 
+    page.setDefaultNavigationTimeout(0);    
+    const response = await page.goto(enlace); 
+    const body     = await response.text();   
+            
+    const { window: { document } } = new jsdom.JSDOM(body); 
+        
+    var filtroHTML  = document.getElementById("rev-linea");
+    var filtro2HTML = filtroHTML.getElementsByClassName("table-resultadosFicha")[0];
+    const tabla1    = filtro2HTML.querySelectorAll("tbody tr td");
+    const titulo    = tabla1[0].textContent;
+    const issn      = tabla1[tabla1.length-1].textContent;
+
+    filtroHTML      = document.getElementById("datos-comunes");
+    const tabla2    = filtroHTML.querySelectorAll("div table tbody tr td");
+    const tabla3    = filtroHTML.querySelectorAll("div table tbody tr th");
+
+    var moverPosicion = 0;
+    var organismoResponsable = null;
+    if(tabla3[3].textContent == "Organismo responsable:") // No todas las revistas tienen esta variable
+    {
+      moverPosicion = 1;
+      organismoResponsable = tabla2[3].textContent.trim().replaceAll(";", ",");
+    }
+
+    const idioma    = tabla2[0].textContent.trim().replaceAll(";", ",");
+    const tema      = tabla2[1].textContent.trim().replaceAll(";", ",");
+    const subtemas  = tabla2[2].textContent.trim().replaceAll(";", ",");
+    const editorial = tabla2[3+moverPosicion].textContent.trim().replaceAll(";", ",");
+    const ciudad    = tabla2[4+moverPosicion].textContent.trim().replaceAll(";", ",");
+    const provincia = tabla2[5+moverPosicion].textContent.trim().replaceAll(";", ","); // En Latindex se llama Estado/Provincia/Departamento pero para hacerlo corto lo llamo directamente provincia
+    const correo    = tabla2[6+moverPosicion].textContent.trim().replaceAll(";", ",");
+
+    await browser.close();  
+    
+    console.log(`***********************************************************************************`);
+    console.log(`Título: ${titulo}`);
+    console.log(`ISSN: ${issn}`);
+    console.log(`Idioma: ${idioma}`);
+    console.log(`Tema: ${tema}`);
+    console.log(`Subtemas: ${subtemas}`);
+    console.log(`Organismo responsable: ${organismoResponsable}`);
+    console.log(`Editorial: ${editorial}`);
+    console.log(`Ciudad: ${ciudad}`);
+    console.log(`Estado/Provincia/Departamento: ${provincia}`);
+    console.log(`Correo: ${correo}`);
+    console.log(`***********************************************************************************`);
+
+    return `${titulo};${issn};${idioma};${tema};${subtemas};${organismoResponsable};${editorial};${ciudad};${provincia};${correo};` + `\n`;
+  }
+  catch (error) 
+  {
+    console.error(error);
+  }
+}
+
+
+// Extraigo la info de todas las revistas de la consulta
+async function extraerInfoLatindex(puppeteer, jsdom, fs)
+{
+  console.log("Comienza la extracción de datos de Latindex");
+
+  const enlaces  =  await buscarEnlacesARevistas(puppeteer, jsdom);
+  var info       = "Título;ISSN;Idioma;Tema;Subtemas;Organismo responsable;Editorial;Ciudad;Estado/Provincia/Departamento;Correo;" + "\n";
+
+  console.log("CANTIDAD DE REVISTAS: " + enlaces.length);
+  
+  var i;
+  for(i = 0; i < enlaces.length-1; i++){
+    console.log(`REVISTA ${i}`);
+    info += await extraerInfoRevista(puppeteer, jsdom, enlaces[i]);
+  }
+
+
+  // Escribimos la info de cada revista en el archivo excel. En caso de que ya exista el archivo, lo reescribe así tenemos siempre la información actualizada
+  fs.writeFile('./Revistas/Latindex.xls', info, error => 
+  { 
+    if(error) console.log(error);
+  })
+
+  console.log("Termina la extracción de datos de Latindex");
 }
 
 
